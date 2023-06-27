657ed5386eecd878b4b0b9d38dd86a091af0afa2
RBGAMES <RBGAMES0220@gmail.com>
2023-06-27 12:00:35 +0800

    20230627

--- FLIES/Assets/程式/Glory/Enemy.cs
+++ FLIES/Assets/程式/Glory/Enemy.cs
@@ -13,24 +13,32 @@ public class Enemy : MonoBehaviour
     private Transform target; // 玩家的參考點或物件
     public float threshold = 0.1f; // 到達目標點的距離閾值
     public float detectionRadius = 5f; // 偵測半徑
+    public float detectionInterval = 0.2f; // 偵測間隔
+    public float attackDistance = 2f; // 攻擊距離
+    private Animator animator;
 
     private enum EnemyState
     {
         Patrol, // 巡邏狀態
-        Chase // 追逐狀態
+        Chase, // 追逐狀態
+        Attack // 攻擊狀態
     }
 
     private EnemyState currentState;
+    private float detectionTimer = 0f;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         target = GameObject.FindGameObjectWithTag("Player").transform; // 獲取玩家的參考點或物件
         currentState = EnemyState.Patrol;
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
+        detectionTimer += Time.deltaTime;
+
         switch (currentState)
         {
             case EnemyState.Patrol:
@@ -38,6 +46,9 @@ public class Enemy : MonoBehaviour
                 break;
             case EnemyState.Chase:
                 Chase(); // 追逐行為
+                break;
+            case EnemyState.Attack:
+                Attack(); // 攻擊行為
                 break;
         }
     }
@@ -73,19 +84,18 @@ public class Enemy : MonoBehaviour
             if (ShouldChase())
             {
                 currentState = EnemyState.Chase;
+                animator.SetBool("isChasing", true); // 開始追逐動畫
             }
         }
     }
 
     private void Chase()
     {
-        // 判斷敵人與玩家的距離
-        float distanceToTarget = Vector3.Distance(transform.position, target.position);
-
         // 判斷是否超出偵測範圍
-        if (distanceToTarget > detectionRadius)
+        if (Vector3.Distance(transform.position, target.position) > detectionRadius)
         {
             currentState = EnemyState.Patrol; // 超出範圍則切換回巡邏狀態
+            animator.SetBool("isChasing", false); // 停止追逐動畫
             return;
         }
 
@@ -103,6 +113,55 @@ public class Enemy : MonoBehaviour
         else if (direction.x < 0 && isFacingRight)
         {
             Flip(); // 翻轉面朝方向
+        }
+
+        // 檢查是否進入攻擊距離
+        if (Vector3.Distance(transform.position, target.position) < attackDistance)
+        {
+            currentState = EnemyState.Attack;
+            animator.SetBool("isChasing", false); // 停止追逐動畫
+            animator.SetBool("isAttacking", true); // 開始攻擊動畫
+        }
+    }
+
+    private void Attack()
+    {
+        // 攻擊行為，可以根據需求自行實現
+
+        // 檢查是否超出偵測範圍
+        if (Vector3.Distance(transform.position, target.position) > detectionRadius)
+        {
+            currentState = EnemyState.Patrol; // 超出範圍則切換回巡邏狀態
+            animator.SetBool("isAttacking", false); // 停止攻擊動畫
+            return;
+        }
+
+        // 判斷敵人與玩家的相對位置，決定移動方向
+        Vector3 direction = (target.position - transform.position).normalized;
+
+        // 移動向玩家位置（可以根據需求自行實現攻擊行為）
+
+        // 根據移動方向來更新面朝方向
+        if (direction.x > 0 && !isFacingRight)
+        {
+            Flip(); // 翻轉面朝方向
+        }
+        else if (direction.x < 0 && isFacingRight)
+        {
+            Flip(); // 翻轉面朝方向
+        }
+
+        // 檢查是否超出攻擊距離
+        if (Vector3.Distance(transform.position, target.position) > attackDistance)
+        {
+            currentState = EnemyState.Chase; // 超出攻擊距離則切換回追逐狀態
+            animator.SetBool("isAttacking", false); // 停止攻擊動畫
+            animator.SetBool("isChasing", true); // 開始追逐動畫
+        }
+        else 
+        {
+            animator.SetBool("isAttacking", true);
+            animator.SetBool("isChasing", false);
         }
     }
 
@@ -124,6 +183,11 @@ public class Enemy : MonoBehaviour
         transform.localScale = scale;
     }
 }
+
+
+
+
+
 
 
 
