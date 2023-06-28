ffd56a322b0555a816cbb5afb85e73c322bb9b51
RBGAMES <RBGAMES0220@gmail.com>
2023-06-28 11:40:17 +0800

    20230628

---
+++ FLIES/Assets/程式/Glory/SpikeTrap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrap : MonoBehaviour
+{
+    public int damageAmount = 10; // 地刺造成的傷害值
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // 檢查觸發地刺陷阱的對象是否是玩家
+        PlayerHealth Player = other.GetComponent<PlayerHealth>();
+        if (Player != null)
+        {
+            // 對玩家造成傷害
+            Player.TakeDamage(damageAmount);
+        }
+    }
+}
