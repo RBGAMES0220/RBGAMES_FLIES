1453e7b8531105aefedcd205ac7188b1ca923867
RBGAMES <RBGAMES0220@gmail.com>
2023-06-29 11:24:50 +0800

    20230629

--- FLIES/Assets/程式/Glory/SpikeTrap.cs
+++ FLIES/Assets/程式/Glory/SpikeTrap.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class SpikeTrap : MonoBehaviour
 {
     public int damageAmount = 10; // 地刺造成的傷害值
+    public float damageInterval = 1f; // 傷害間隔時間
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private float damageTimer; // 傷害計時器
+
+    private void OnTriggerStay2D(Collider2D other)
     {
         // 檢查觸發地刺陷阱的對象是否是玩家
-        PlayerHealth Player = other.GetComponent<PlayerHealth>();
-        if (Player != null)
+        PlayerHealth player = other.GetComponent<PlayerHealth>();
+        if (player != null)
         {
-            // 對玩家造成傷害
-            Player.TakeDamage(damageAmount);
+            // 更新傷害計時器
+            damageTimer += Time.deltaTime;
+
+            // 檢查是否達到傷害間隔時間
+            if (damageTimer >= damageInterval)
+            {
+                // 對玩家造成傷害
+                player.TakeDamage(damageAmount);
+
+                // 重置傷害計時器
+                damageTimer = 0f;
+            }
         }
     }
 }
+
